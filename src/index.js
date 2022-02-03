ad60784e4c2b2d9f40bb27c84b8f3bd62f1e0bbf
Abd-Beltaji <76127880+Abd-Beltaji@users.noreply.github.com>
2022-02-03 17:40:56 +0200

    Enhanced the functionality to avoid using "Eval"
    Simplified some of the original code, and changed it to not rely on "Eval" when calculating the result. This has been done by using multiple states and handling them independently. Used "useCallback" and "useEffect" for performance enhancement reasons.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,68 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./style.css";
+import React, { useState, useCallback, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
 
 function App() {
-  const [text, setText] = React.useState("");
-  const buttonContent = [
-    "1",
-    "2",
-    "3",
-    "=",
-    "4",
-    "5",
-    "6",
-    "/",
-    "7",
-    "8",
-    "9",
-    "*",
-    "C",
-    "0",
-    "+",
-    "-",
-  ];
-  return (
-    <div className="container">
-      <div className="screen">
-        <input className="result" type="text" disabled value={text} />
-      </div>
-      <div className="btns">
-        {buttonContent.map((e, index) => (
-          <span
-            className={isNaN(parseInt(e)) ? "operation" : "number"}
-            key={index}
-            onClick={(event) => {
-              if (event.target.textContent === "C")
-                setText((prevText) => prevText.slice(0, -1));
-              else if (event.target.textContent === "=")
-                setText(eval(text).toString());
-              else setText((prev) => prev + event.target.textContent);
-            }}
-          >
-            {e}
-          </span>
-        ))}
-      </div>
-    </div>
-  );
+	const buttonContent = [...`123=456/789*C0+-`]
+
+	const [n1, setN1] = useState(null)
+	const [n2, setN2] = useState(null)
+	const [operation, setOperation] = useState(null)
+	const [result, setResult] = useState(null)
+
+	const [display, setDisplay] = useState('')
+
+	const unNull = useCallback((x, y) => (x !== null ? x : y))
+	useEffect(
+		() =>
+			setDisplay(
+				unNull(result, operation === null ? unNull(n1, '') : unNull(n2, ''))
+			),
+		[n1, n2, result, operation]
+	)
+	const calculate = useCallback((n1, n2, op) =>
+		op === '+' ? n1 + n2
+			: op === '*' ? n1 * n2
+			: op === '/' ? n1 / n2
+			: op === '-' ? n1 - n2
+			: 0
+	)
+	return (
+		<div className="container">
+			<div className="screen">
+				<input className="result" type="text" disabled value={display} />
+			</div>
+			<div className="btns">
+				{buttonContent.map(e => (
+					<span
+						type={/[0-9]/g.test(e) ? 'number' : 'operation'}
+						key={e}
+						value={e}
+						onClick={evt => {
+							const v = evt.target.getAttribute('value')
+							if (v === 'C') {
+								setN1(null); setN2(null); setOperation(null);	setResult(null)
+							} else if (v === '=') {
+								const res = calculate(n1, n2, operation)
+								setResult(res); setN1(res)
+								setN2(null); setOperation(null)
+							} else if (evt.target.getAttribute('type') === 'number') {
+								setResult(null)
+								const clickedValue = Number(v)
+								if (operation !== null)
+									setN2(n2 === null ? clickedValue : n2 * 10 + clickedValue)
+								else setN1(n1 === null ? clickedValue : n1 * 10 + clickedValue)
+							} else {
+								setResult(null); setOperation(v)
+							}
+						}}
+					>
+						{e}
+					</span>
+				))}
+			</div>
+		</div>
+	)
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
